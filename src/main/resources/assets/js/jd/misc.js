331176b09d225289220506c377144b35a3803d1e
Ganesh Gowtham G R <ganesh.gowtham@gmail.com>
2017-03-08 18:45:12 +0530

    recptionist landing page

--- src/main/resources/assets/js/jd/misc.js
+++ src/main/resources/assets/js/jd/misc.js
@@ -1,7 +1,7 @@
 /* Main screen login */
 function login() {
     console.log("Login called  " + NATIVE_LOGIN);
-    fetch(NATIVE_LOGIN, {
+    /*fetch(NATIVE_LOGIN, {
         method: 'get'
         , mode: 'cors'
         , redirect: 'follow'
@@ -11,11 +11,20 @@ function login() {
     }).catch(function (err) {
         // Error :(
         console.log(err);
-    });
+    });*/
+    var loginUserType = document.getElementById('loginUserType').value;
+    console.log(loginUserType);
+    if(loginUserType == 'patient'){
+        window.location.href = "/html/patient/patientlanding.html";
+    } else if (loginUserType == 'receptionist') {
+        window.location.href = "/html/receptionist/receptionistlanding.html";
+    }
+    
 }
 /* Main screen regstration */
 function register() {
     var loginId = document.getElementById('loginId').value;
+    
     var password = document.getElementById('password').value;
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
