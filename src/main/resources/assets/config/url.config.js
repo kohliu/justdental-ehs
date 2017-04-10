ce5ad29821307978efdb24cd28ca781ea672ab0c
Ganesh Gowtham G R <ganesh.gowtham@gmail.com>
2017-04-11 01:05:31 +0530

    rev

--- src/main/resources/assets/config/url.config.js
+++ src/main/resources/assets/config/url.config.js
@@ -1,15 +1,16 @@
 //LAB and Live
-var environment = 'LAB';
+var environment = 'LIVE';
 
 if(environment == 'LAB')
 {
     HOSTNAME = 'http://ec2-52-66-11-22.ap-south-1.compute.amazonaws.com:8080';
+    console.info('Starting in '+environment+'  '+HOSTNAME);
 }
 else if (environment == 'LIVE')
 {
     HOSTNAME = 'http://ec2-52-42-219-206.us-west-2.compute.amazonaws.com:8080';
 }
-console.info('Starting in '+environment+'  '+HOSTNAME);
+
 var NATIVE_LOGIN = HOSTNAME + '/api/jdclinic';
 var QUICK_REGISTER = HOSTNAME + '/api/jdnativelogin'; 
 var SUBSCRIBE = HOSTNAME + '/api/jdsubscribe'; 
