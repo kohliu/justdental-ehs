f17ba20ce8c72f9bcbcf32ccf8a2093d08f1afd3
Ganesh Gowtham G R <ganesh.gowtham@gmail.com>
2017-06-03 20:51:35 +0530

    routes

--- src/main/resources/assets/js/jd/routes.js
+++ src/main/resources/assets/js/jd/routes.js
@@ -1,17 +1,39 @@
-function loadDoctorLandingPage()
-{   
-    $( "#mainBody" ).load( "/html/doctor/doctorlanding.html", function() {
-        initDoctorLandingPage();
-        return false;
-   });
-    return false;
+/*
+Routes and page re-direction will declared there.
+*/
+
+function doNothing()
+{
+
 }
 
-function loadAdminLandingPage()
-{   
-    $( "#mainBody" ).load( "/html/admin/adminlanding.html", function() {
-        initAdminLandingPage();
-        return false;
-   });
+function loadDoctorLandingPage() {    
+    return replaceBodyContent('/html/doctor/doctorlanding.html', initDoctorLandingPage);
+}
+
+function loadAdminLandingPage() {    
+    return replaceBodyContent('/html/admin/adminlanding.html', initAdminLandingPage);
+}
+
+function loadMultiProfilePage() {
+    function hideLoginModal()
+    {
+        $('#loginModal').modal('hide');
+    }    
+    return replaceBodyContent('/html/misc/multi-profile.html', hideLoginModal);
+}
+
+function loadIndexPage()
+{
+    return replaceBodyContent('index-main.html', doNothing);
+}
+
+function replaceBodyContent(pageURI, funcRef)
+{
+    console.log('replaceBodyContent  ',pageURI, funcRef);
+    $("#bodyContent").load(pageURI, function () {
+        funcRef();
+         return false;
+    });
     return false;
 }
